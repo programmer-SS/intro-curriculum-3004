86ad2846d77bbf28f38054e623281e0813dac2c8
programmer-SS <167070988+programmer-SS@users.noreply.github.com>
2024-05-04 13:05:48 +0900

    multi機能の追加

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,14 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+module.exports = { 
+  add,
+  multi
+ };
